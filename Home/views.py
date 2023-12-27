74f20f30248c9b2dada51b597e25176875259872
Shiwani Kadu <init.defaultBranch>
2023-12-27 19:49:32 +0530

    .

--- Home/views.py
+++ Home/views.py
@@ -5,7 +5,9 @@ from django.contrib import messages
 
 # Create your views here.
 def index(request):
+    messages.success(request,"This is a test messages")
     return render(request, "index.html")
+    
    
 def about(request):
     #return HttpResponse("This is about page")
@@ -17,14 +19,14 @@ def services(request):
 
 def contact(request):
     #return HttpResponse("This is contact page")
-    if request.method=="POST":
+    if request.method =="POST":
         name=request.POST.get("name")
         email=request.POST.get("email")
         phone=request.POST.get("phone")
         desc=request.POST.get("desc")
         contact=Contact(name=name,email=email,phone=phone,desc=desc,date=datetime.today())
         contact.save()
-        messages.success(request,"Your message has been sent")
+        messages.success(request,"Your message has been sent!")
        
 
     return render(request, "contact.html")
