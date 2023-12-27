4993e09c0fe53a10493126430948d48f7454ffa4
Shiwani Kadu <init.defaultBranch>
2023-12-27 19:18:41 +0530

    .

--- Home/views.py
+++ Home/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,HttpResponse
 from datetime import datetime
 from Home.models import Contact
-from django.contrib.messages import messages
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
@@ -24,6 +24,7 @@ def contact(request):
         desc=request.POST.get("desc")
         contact=Contact(name=name,email=email,phone=phone,desc=desc,date=datetime.today())
         contact.save()
+        messages.success(request,"Your message has been sent")
        
 
     return render(request, "contact.html")
